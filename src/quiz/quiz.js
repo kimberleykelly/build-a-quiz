613328f8ba0efc60f07ac0f78f9fb4de51dd20ec
Kimberley Kelly <kimberley.b.kelly@gmail.com>
2021-04-29 21:34:10 +0100

    add useState hook to keep track of the currentQuestion

--- src/quiz/quiz.js
+++ src/quiz/quiz.js
@@ -1,20 +1,30 @@
 import questions from "./questions.json"
+import { useState } from "react"
 
 const Quiz = () => {
+  const [currentQuestion, setCurrentQuestion] = useState(0)
+
+  const handleNextButtonClick = () => {
+    setCurrentQuestion(currentQuestion + 1)
+  }
+
   return (
     <div className="quiz-container">
       <div className="quiz-title">The Modern Family Quiz!</div>
       <div className="quiz-question">
-        <i>{questions[0].text}</i>
+        <i>{questions[currentQuestion].text}</i>
       </div>
       <div className="quiz-options">
-        {questions[0].options.map((option, index) => (
+        {questions[currentQuestion].options.map((option, index) => (
           <label key={index} className="quiz-question-option">
             <input type="radio" value={option} />
             <div className="quiz-option">{option}</div>
           </label>
         ))}
       </div>
+      <button className="next-btn" onClick={() => handleNextButtonClick()}>
+        NEXT
+      </button>
     </div>
   )
 }
