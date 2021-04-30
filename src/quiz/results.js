a72f0d4704a76019902fbfc30a2913d91fd380a0
Kimberley Kelly <kimberley.b.kelly@gmail.com>
2021-04-30 09:13:40 +0100

    display results to the user

--- src/quiz/results.js
+++ src/quiz/results.js
@@ -1,7 +1,23 @@
-const Results = () => {
+const Results = ({ questions, results }) => {
   return (
     <div className="results-container">
       <div className="results-title">Results!</div>
+      {questions.map((question, index) => (
+        <div className="results-summary-container" key={index}>
+          <div className="results-question">
+            <strong>Question: </strong>
+            {question.text}
+          </div>
+          <div className="results-user-answer">
+            <strong>Your Answer: </strong>
+            <i>{results[index]} </i>
+          </div>
+          <div className="quiz-correct-answer">
+            <strong>Correct Answer: </strong>
+            {question.correct_answer}
+          </div>
+        </div>
+      ))}
     </div>
   )
 }
