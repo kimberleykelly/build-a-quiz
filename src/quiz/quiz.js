f133a4850f39939da5bc52670a11a6c843e2168b
Kimberley Kelly <kimberley.b.kelly@gmail.com>
2021-04-30 09:02:54 +0100

    display results component when results button is clicked

--- src/quiz/quiz.js
+++ src/quiz/quiz.js
@@ -1,9 +1,11 @@
 import questions from "./questions.json"
 import { useState } from "react"
+import Results from "./results"
 
 const Quiz = () => {
   const [currentQuestion, setCurrentQuestion] = useState(0)
   const [results, setResults] = useState({})
+  const [isComplete, setIsComplete] = useState(false)
 
   const isLastQuestion = currentQuestion === questions.length - 1
 
@@ -16,34 +18,46 @@ const Quiz = () => {
   }
 
   return (
-    <div className="quiz-container">
-      <div className="quiz-title">The Modern Family Quiz!</div>
-      <div className="quiz-current-q">
-        {currentQuestion + 1}/{questions.length}
-      </div>
-      <div className="quiz-question">
-        <i>{questions[currentQuestion].text}</i>
-      </div>
-      <div className="quiz-options">
-        {questions[currentQuestion].options.map((option, index) => (
-          <label key={index} className="quiz-question-option">
-            <input
-              type="radio"
-              value={option}
-              checked={results[currentQuestion] === option}
-              onChange={(e) => handleOptionClick(e)}
-            />
-            <div className="quiz-option">{option}</div>
-          </label>
-        ))}
-      </div>
-      {!isLastQuestion && (
-        <button className="next-btn" onClick={() => handleNextButtonClick()}>
-          NEXT
-        </button>
+    <>
+      {!isComplete && (
+        <div className="quiz-container">
+          <div className="quiz-title">The Modern Family Quiz!</div>
+          <div className="quiz-current-q">
+            {currentQuestion + 1}/{questions.length}
+          </div>
+          <div className="quiz-question">
+            <i>{questions[currentQuestion].text}</i>
+          </div>
+          <div className="quiz-options">
+            {questions[currentQuestion].options.map((option, index) => (
+              <label key={index} className="quiz-question-option">
+                <input
+                  type="radio"
+                  value={option}
+                  checked={results[currentQuestion] === option}
+                  onChange={(e) => handleOptionClick(e)}
+                />
+                <div className="quiz-option">{option}</div>
+              </label>
+            ))}
+          </div>
+          {!isLastQuestion && (
+            <button
+              className="next-btn"
+              onClick={() => handleNextButtonClick()}
+            >
+              NEXT
+            </button>
+          )}
+          {isLastQuestion && (
+            <button className="results-btn" onClick={() => setIsComplete(true)}>
+              GET RESULTS
+            </button>
+          )}
+        </div>
       )}
-      {isLastQuestion && <button className="results-btn">GET RESULTS</button>}
-    </div>
+      {isComplete && <Results />}
+    </>
   )
 }
 
